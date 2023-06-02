cd7bc1212278c96927554d726f121a31e29e1ab8
Igor Todorovski <itodorov@ca.ibm.com>
2023-06-02 12:49:25 -0400

    Fix cicd

--- cicd.groovy
+++ cicd.groovy
@@ -1,5 +1,13 @@
 node('linux')
 {
+        stage ('Poll') {
+                checkout([
+                        $class: 'GitSCM',
+                        branches: [[name: '*/main']],
+                        doGenerateSubmoduleConfigurations: false,
+                        extensions: [],
+                        userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/byaccport.git']]])
+        }
   stage('Build') {
     build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/byaccport.git'), string(name: 'PORT_DESCRIPTION', value: 'Berkeley Yacc (byacc) is generally conceded to be the best yacc variant available' )]
   }
