b536f74472e0135f75f2dd8b14f403e7cc7968ca
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:21:48 -0400

    Change organization to zopencommunity

--- cicd.groovy
+++ cicd.groovy
@@ -6,9 +6,9 @@ node('linux')
                         branches: [[name: '*/main']],
                         doGenerateSubmoduleConfigurations: false,
                         extensions: [],
-                        userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/byaccport.git']]])
+                        userRemoteConfigs: [[url: 'https://github.com/zopencommunity/byaccport.git']]])
         }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/byaccport.git'), string(name: 'PORT_DESCRIPTION', value: 'Berkeley Yacc (byacc) is generally conceded to be the best yacc variant available' )]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/byaccport.git'), string(name: 'PORT_DESCRIPTION', value: 'Berkeley Yacc (byacc) is generally conceded to be the best yacc variant available' )]
   }
 }
